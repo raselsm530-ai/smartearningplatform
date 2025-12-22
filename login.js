cc692edd597a83c3679f192a49cada03a38fc8c0
raselsm530-ai <raselsm530@gmail.com>
2025-12-22 15:39:05 +0600

    Update login.js

--- login.js
+++ login.js
@@ -1,17 +1,21 @@
 import { auth } from "./firebase-config.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
 window.login = () => {
     const phone = document.getElementById("phone").value.trim();
-    const pass = document.getElementById("password").value.trim();
-    if (!phone || !pass) { alert("সব ঘর পূরণ করুন"); return; }
+    const password = document.getElementById("password").value.trim();
+
+    if (!phone || !password) { alert("সব ঘর পূরণ করুন"); return; }
 
     const email = phone + "@app.com";
-    signInWithEmailAndPassword(auth, email, pass)
-        .then(() => {
-            localStorage.setItem("user", phone);
-            alert("লগইন সফল 🎉");
-            location.href = "home.html";
-        })
-        .catch(err => alert("❌ লগইন ব্যর্থ: ভুল নম্বর বা পাসওয়ার্ড"));
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        localStorage.setItem("currentUser", phone);
+        alert("লগইন সফল!");
+        location.href = "home.html";
+    })
+    .catch((error) => {
+        alert("লগইন ব্যর্থ: ভুল নম্বর বা পাসওয়ার্ড");
+    });
 };
